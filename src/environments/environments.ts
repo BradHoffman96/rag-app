63a8a003125ac889338ffd6a5448786e8ef4db4f
Brad Hoffman <bradleydavidhoffman@gmail.com>
2018-03-06 20:20:36 -0500

    updated project and added Firebase config to environments folder

---
+++ src/environments/environments.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD-oE3GXWIjci3DE_X0dbgGFDLwDZUgUeQ",
+    authDomain: "rag-app.firebaseapp.com",
+    databaseURL: "https://rag-app.firebaseio.com",
+    projectId: "rag-app",
+    storageBucket: "",
+    messagingSenderId: "244073414730"
+  };
