ff37ecfe8a14c2451b78b3e7dc9e684552cd93db
Brad Hoffman <bradleydavidhoffman@gmail.com>
2018-03-06 21:22:23 -0500

    logout is now functioning

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-profile',
@@ -8,7 +10,17 @@ import { NavController } from 'ionic-angular';
 export class ProfilePage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private afAuth: AngularFireAuth) {
   }
-  
+
+  logout() {
+    this.afAuth.auth.signOut()
+      .then(result => {
+        console.log(result);
+        this.navCtrl.setRoot(LoginPage);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }  
 }
