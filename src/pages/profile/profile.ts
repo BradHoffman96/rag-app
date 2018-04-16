331ea8e029913a05570960d665693447f835ce34
Brad Hoffman <bradleydavidhoffman@gmail.com>
2018-04-16 15:26:05 -0400

    displayName can now be viewed on the profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { LoginPage } from '../login/login';
+import { User } from '../../shared/models/user';
 
 @Component({
   selector: 'page-profile',
@@ -10,7 +11,14 @@ import { LoginPage } from '../login/login';
 export class ProfilePage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
+  user = {} as User;
+
   constructor(public navCtrl: NavController, private afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.user.name = user.displayName;
+      }
+    })
   }
 
   logout() {
